44d3c4ce5695dc42ed571f8c0589a75b1df92f6c
Harmanen <hurmanen@gmail.com>
2026-03-01 11:42:26 +0200

    Fix syntax for broken reactivity

--- app.R
+++ app.R
@@ -47,108 +47,123 @@ server <- function(input, output) {
   # Update url, fetch achievements, and render outputs when submit button is
   # pressed
   observeEvent(input$steam_username, {
-    url <- base::paste0(
+    url(base::paste0(
       "https://steamcommunity.com/id/",
       isolate(input$steam_username),
       "/stats/686060/achievements/"
-    )
+    ))
 
     # Scrape achievement elements from the page and convert to text
-    achievements <- url |>
-      rvest::read_html() |>
-      rvest::html_elements(".achieveRow") |>
-      rvest::html_text2()
+    achievements(
+      url() |>
+        rvest::read_html() |>
+        rvest::html_elements(".achieveRow") |>
+        rvest::html_text2()
+    )
 
-    if (base::length(achievements) > 0) {
+    if (base::length(achievements()) > 0) {
       # Select unlocked achievements
-      unlocked_achievements <- achievements[
-        grepl("Unlocked\\s\\d", achievements)
-      ]
+      unlocked_achievements(
+        achievements()[
+          grepl("Unlocked\\s\\d", achievements())
+        ]
+      )
 
       # Generate a data frame from the unlocked achievements.
-      data_achievements_all <- dplyr::bind_rows(
-        base::lapply(
-          unlocked_achievements,
-          function(achievement) {
-            # "House upgrade 1\nSend Frank 1 cat.\nUnlocked 10 Feb @ 11:19am\n"
-            splits <- base::strsplit(achievement, "\n") |> base::unlist()
+      data_achievements_all(
+        dplyr::bind_rows(
+          base::lapply(
+            unlocked_achievements(),
+            function(achievement) {
+              # "House upgrade 1\nSend Frank 1 cat.\nUnlocked 10 Feb @ 11:19am\n"
+              splits <- base::strsplit(achievement, "\n") |> base::unlist()
 
-            base::data.frame(
-              Name = splits[[1]],
-              Description = splits[[2]],
-              Time = splits[[3]]
-            )
-          }
+              base::data.frame(
+                Name = splits[[1]],
+                Description = splits[[2]],
+                Time = splits[[3]]
+              )
+            }
+          )
         )
       )
 
       # Keep relevant achievements
-      data_achievements_filtered <- data_achievements_all |>
-        dplyr::filter(
-          stringr::str_detect(Description, "Complete the \\w+ with the \\w+")
-        )
+      data_achievements_filtered(
+        data_achievements_all() |>
+          dplyr::filter(
+            stringr::str_detect(Description, "Complete the \\w+ with the \\w+")
+          )
+      )
 
       # Split Description to Area and Class columns
-      descriptions <- data_achievements_filtered |> dplyr::pull(Description)
+      descriptions(data_achievements_filtered() |> dplyr::pull(Description))
 
-      data_achievements_filtered$Area <- purrr::map_vec(
-        descriptions,
-        function(description) {
-          description |>
-            stringr::str_replace("Complete the ", "") |>
-            stringr::str_replace(" with the \\w+", "") |>
-            stringr::str_replace("\\.", "")
-        }
-      )
-
-      data_achievements_filtered$Class <- purrr::map_vec(
-        descriptions,
-        function(description) {
-          description |>
-            stringr::str_replace("Complete the \\w+", "") |>
-            stringr::str_replace(" with the ", "") |>
-            stringr::str_replace("\\.", "")
-        }
-      )
-
-      # Extract Date from unlock time
-      data_achievements_filtered$Date <- purrr::map_vec(
-        data_achievements_filtered |> dplyr::pull(Time),
-        function(time) {
-          time |>
-            stringr::str_replace("Unlocked ", "") |>
-            stringr::str_replace(" @ \\d+:\\d+(a|p)m", "")
-        }
-      )
-
-      if (base::nrow(data_achievements_filtered) > 0) {
-        data_achievements_filtered <- data_achievements_filtered |>
-          # Append current year for rows missing a year
+      data_achievements_filtered(
+        data_achievements_filtered() |>
           dplyr::mutate(
-            Date = dplyr::case_when(
-              base::nchar(Date) == 6 ~
-                base::paste0(Date, ", ", base::Sys.time() |> base::strtrim(4)),
-              .default = Date
+            Area = purrr::map_vec(
+              descriptions(),
+              function(description) {
+                description |>
+                  stringr::str_replace("Complete the ", "") |>
+                  stringr::str_replace(" with the \\w+", "") |>
+                  stringr::str_replace("\\.", "")
+              }
+            ),
+            Class = purrr::map_vec(
+              descriptions(),
+              function(description) {
+                description |>
+                  stringr::str_replace("Complete the \\w+", "") |>
+                  stringr::str_replace(" with the ", "") |>
+                  stringr::str_replace("\\.", "")
+              }
+            ),
+            # Extract Date from unlock time
+            Date = purrr::map_vec(
+              data_achievements_filtered() |> dplyr::pull(Time),
+              function(time) {
+                time |>
+                  stringr::str_replace("Unlocked ", "") |>
+                  stringr::str_replace(" @ \\d+:\\d+(a|p)m", "")
+              }
             )
-          ) |>
-          # Standardize timestamp
-          dplyr::mutate(Date = lubridate::dmy(Date))
+          )
+      )
+
+      if (base::nrow(data_achievements_filtered()) > 0) {
+        data_achievements_filtered(
+          data_achievements_filtered() |>
+            # Append current year for rows missing a year
+            dplyr::mutate(
+              Date = dplyr::case_when(
+                base::nchar(Date) == 6 ~
+                  base::paste0(Date, ", ", base::Sys.time() |> base::strtrim(4)),
+                .default = Date
+              )
+            ) |>
+            # Standardize timestamp
+            dplyr::mutate(Date = lubridate::dmy(Date))
+        )
 
         # Pivot to sensible format
-        data_class_table <- data_achievements_filtered |>
-          dplyr::select(Area, Class, Date) |>
-          # Dates are rendered as unix time for some reason... Convert
-          dplyr::mutate(Date = base::as.character(Date)) |>
-          tidyr::pivot_wider(names_from = Class, values_from = Date)
+        data_class_table(
+          data_achievements_filtered() |>
+            dplyr::select(Area, Class, Date) |>
+            # Dates are rendered as unix time for some reason... Convert
+            dplyr::mutate(Date = base::as.character(Date)) |>
+            tidyr::pivot_wider(names_from = Class, values_from = Date)
+        )
 
         output$class_table <- renderTable(
-          data_class_table,
+          data_class_table(),
           caption = "Dates for areas finished per class",
           caption.placement = "top"
         )
 
         output$achievements <- renderTable(
-          data_achievements_all,
+          data_achievements_all(),
           caption = "All unlocked achievements",
           caption.placement = "top"
         )
